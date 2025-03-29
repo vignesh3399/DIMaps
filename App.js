f7cda63d2381ecd619ed0f6d564ad34e630fb12e
vignesh3399 <vigneshwarreddykarra@gmail.com>
2025-03-29 13:01:38 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from "react";
+import Map from "./components/map";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>DIMAPS: Accessible Navigation</h1>
+      <Map />
+    </div>
+  );
+}
+
+export default App;
